c8c14eb8f94bc9d2daaec33ffd519e9788899594
Jose Marcelo Carvalho Filho <jmcarvalho05@hotmail.com>
2026-03-04 12:15:07 -0500

    tf-practical: New practicals from Zeal

---
+++ aws/04-Variables/variables.tf
@@ -0,0 +1,15 @@
+variable "vpn_ip" {
+    default = "104.203.46.2/32"
+}
+
+variable "app_port" {
+    default = "8080"
+}
+
+variable "ssh_port" {
+    default = "22"
+}
+
+variable "ftp_port" {
+    default = "21"
+}
